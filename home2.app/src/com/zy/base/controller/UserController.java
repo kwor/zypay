fc059be63fe1f0b6cd6dbbade089ca360b4cbd70
thinkpad-PC\thinkpad <kwor.wang@gmail.com>
2017-04-06 17:07:26 +0800

    Restful url get userinfo

--- home2.app/src/com/zy/base/controller/UserController.java
+++ home2.app/src/com/zy/base/controller/UserController.java
@@ -2,12 +2,13 @@ package com.zy.base.controller;
 
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.zy.base.pojo.User;
 import com.zy.base.service.IUserService;
 
@@ -17,14 +18,11 @@ public class UserController {
 	@Resource
 	private IUserService userService;
 	
-	@RequestMapping("/showUser")
-	public String toIndex(HttpServletRequest request,Model model){
-		int userId = Integer.parseInt(request.getParameter("id"));
-		User user = this.userService.getUserById(userId);
-		model.addAttribute("user", user);
-		
-		System.out.println("user:"+user.getUserName());
-		
-		return "showUser";
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public String toIndex(@PathVariable( "id" ) int id){
+		User user = this.userService.getUserById(id);
+
+		return JSON.toJSONString(user);
 	}
 }
