64291e19a90b722eac92713d1ffc45ad6814a1fe
thinkpad-PC\thinkpad <kwor.wang@gmail.com>
2017-04-12 13:17:38 +0800

    根据customerId查询该用户的所有记录列表

--- home2.app/src/com/zy/base/controller/CustomerAccountRechargeController.java
+++ home2.app/src/com/zy/base/controller/CustomerAccountRechargeController.java
@@ -4,19 +4,16 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.alibaba.fastjson.JSON;
-import com.google.common.base.Preconditions;
 
 import com.zy.base.pojo.CustomerAccountRecharge;
+import com.zy.base.pojo.CustomerAccountRechargeExample;
 import com.zy.base.service.ICustomerAccountRechargeService;
 
 @Controller
@@ -33,9 +30,14 @@ public class CustomerAccountRechargeController {
 		return JSON.toJSONString(customerAccountRecharge);
 	}
 	
-	//根据customerId查询记录列表
-	
-	
+	//根据customerId查询该用户的所有记录列表
+	@RequestMapping(value = "/customerid/{customerid}", method = RequestMethod.GET)
+	@ResponseBody
+	public List< CustomerAccountRecharge > findOneById(@PathVariable("customerid") int customerid) {
+		CustomerAccountRechargeExample example=new CustomerAccountRechargeExample();
+		example.createCriteria().andCustomerIdEqualTo(customerid);
+		return customerAccountRechargeService.findAll(example);
+	}
 	
 	/*
 	//查询全部记录列表
